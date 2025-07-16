1004c2398c681847e54bac2a744a30df2a20ea92
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-07-16 14:30:19 +0530

    Update 1.3.1- Add Labels and Images in CustomTkinter.py

--- 1.3.1- Add Labels and Images in CustomTkinter.py
+++ 1.3.1- Add Labels and Images in CustomTkinter.py
@@ -1,33 +1,38 @@
+# Line no 39 loads and resizes the logo.png image to 100×100 pixels using CTkImage for use in a CustomTkinter app.
+
+# Line 45 & 46 creates a label (CTkLabel) in the top_frame to display the logo image without any text and with a 
+# frame_background color, then places it on the left side with padding.
+
 # Task1: Create a label that says "Message Encrypter" on the top frame.
 
 # 🔍 Hint 1: Use CTkLabel to create the label.
 # 🔍 Hint 2: Set text="Message Encrypter", font=("Cascadia Code SemiBold", 20, "bold"), text_color="white", fg_color=background.
-# 🔍 Hint 3: Use .pack(side="left", padx=(100,0)) to place the label with some left padding.
+# 🔍 Hint 3: Use .pack(side="left", padx=(80,0)) to place the label with some left padding.
 
 # Task2: Display an image on the top frame using CTkLabel.
 
 # 🔍 Hint 1: Use CTkImage to load and resize the image. Use: Image.open("OIS.png"), size=(100, 100)
 # 🔍 Hint 2: Use CTkLabel to place the image. Set image=your_image, text="", fg_color=background
-# 🔍 Hint 3: Use .pack(side="left", padx=(10, 10)) to position the image with proper spacing.
+# 🔍 Hint 3: Use .pack(side="left", padx=(70, 10)) to position the image with proper spacing.
 
 
 import customtkinter as ctk
 from PIL import Image, ImageTk  # A library to work with pictures
 
-background = "grey" 
+frame_background = "dimgrey" 
 root = ctk.CTk()
 root.geometry("600x500")
 root.resizable(False, False)
 root.title("Message Encoder")
 
 # Create frames for layout
-top_frame = ctk.CTkFrame(root, fg_color=background, height=80)
+top_frame = ctk.CTkFrame(root, fg_color=frame_background, height=80)
 top_frame.pack(fill="x", pady=5)
 
-middle_frame = ctk.CTkFrame(root, fg_color=background)
+middle_frame = ctk.CTkFrame(root, fg_color=frame_background)
 middle_frame.pack(fill="both", expand=True)
 
-bottom_frame = ctk.CTkFrame(root, fg_color=background, height=100)
+bottom_frame = ctk.CTkFrame(root, fg_color=frame_background, height=100)
 bottom_frame.pack(fill="x")
 
 ##########       Loading Images      ############
@@ -37,10 +42,12 @@ logo = ctk.CTkImage(Image.open('logo.png'), size=(100, 100))  #Open the image an
 
 
 ## Create a label with the logo image, no text, and pack it on the window.
-logo_image = ctk.CTkLabel(top_frame, image=logo, text="", fg_color=background)
-logo_image.pack(side="left", padx=(10, 0))
+logo_image = ctk.CTkLabel(top_frame, image=logo, text="", fg_color=frame_background)
+logo_image.pack(side="left", padx=(25, 0))
 
 #Create a label that says "Message Encrypter" here
+
+
 
 
 
